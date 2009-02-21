832e7a446128df56cd7421de42b7cc789b7b1635
Paul Dix <paul@pauldix.net>
2009-02-21 13:14:09 -0500

    fixed isolated to freeze

--- lib/http-machine/isolated_block.rb
+++ lib/http-machine/isolated_block.rb
@@ -2,14 +2,13 @@ module HTTPMachine
   class IsolatedBlock
     def self.run_isolated(*args, &block)
       
-      new.instance_eval do
-        # make dupes and freeze them
-        instance_variables.each do |variable|
-          instance_variable_set(variable, instance_variable_get(variable).dup.freeze)
-        end
-        
-        instance_eval &block
-      end
+      # make dupes and freeze them
+      args.each {|arg| arg.freeze}
+      # instance_variables.each do |variable|
+      #   instance_variable_set(variable, instance_variable_get(variable).dup.freeze)
+      # end
+      
+      new.instance_eval &block
     end
   end
 end
