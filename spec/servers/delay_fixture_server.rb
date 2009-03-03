a8b635b772648d2eb4f8d344106d764267a8c8bf
Paul Dix <paul@pauldix.net>
2009-03-03 10:04:27 -0500

    updated the delayed fixture server

--- spec/servers/delay_fixture_server.rb
+++ spec/servers/delay_fixture_server.rb
@@ -4,93 +4,56 @@ require 'rubygems'
 require 'eventmachine'
 require 'evma_httpserver'
  
-class Handler  < EventMachine::Connection
+class DelayFixtureServer  < EventMachine::Connection
   include EventMachine::HttpServer
  
   def process_http_request
+    EventMachine.stop if ENV["PATH_INFO"] == "/die"
+    
     resp = EventMachine::DelegatedHttpResponse.new( self )
- 
+    
     # Block which fulfills the request
     operation = proc do
-      sleep 0.1 # simulate a long running request
- 
+      sleep DelayFixtureServer.response_delay
+
       resp.status = 200
-      resp.content = <<-XML
-        <result_set>
-          <ttl>20</ttl>
-          <result>
-            <id>1</id>
-            <name>hello</name>
-            <description>
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-            </description>
-          </result>
-          <result>
-            <id>2</id>
-            <name>hello</name>
-            <description>
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-            </description>
-          </result>
-          <result>
-            <id>3</id>
-            <name>hello</name>
-            <description>
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-              this is a long description for a text field of some kind.
-            </description>
-          </result>
-        </result_set>
-      XML
+      resp.content = DelayFixtureServer.response_fixture
     end
- 
+
     # Callback block to execute once the request is fulfilled
     callback = proc do |res|
       resp.send_response
     end
- 
+
     # Let the thread pool (20 Ruby threads) handle request
     EM.defer(operation, callback)
   end
+  
+  def self.response_fixture
+    @response_fixture ||= ""
+  end
+  
+  def self.response_fixture=(val)
+    @response_fixture = val
+  end
+
+  def self.response_delay
+    @response_delay ||= 0
+  end
+
+  def self.response_delay=(val)
+    @response_delay = val
+  end
 end
- 
-EventMachine::run {
-  EventMachine.epoll
-  EventMachine::start_server("0.0.0.0", ARGV[0].to_i, Handler)
-  puts "Listening..."
-}
+
+# port = (ARGV[0] || 3000).to_i
+# 
+# DelayFixtureServer.response_delay   = (ARGV[1].to_f || 0.1)
+# DelayFixtureServer.response_fixture = ARGV[2].nil? ? nil : File.read(File.dirname(__FILE__) + "/../fixtures/#{ARGV[2]}")
+# 
+# Process.fork do
+#   EventMachine::run {
+#     EventMachine.epoll
+#     EventMachine::start_server("0.0.0.0", port, DelayFixtureServer)
+#   }
+# end
