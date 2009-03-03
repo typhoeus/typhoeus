d8cf4dcd91094866dea822bbc11fc2ddcf259563
Paul Dix <paul@pauldix.net>
2009-03-03 10:08:09 -0500

    updated some todos in the readme and some other possible usage examples

--- examples/possible_usage.rb
+++ examples/possible_usage.rb
@@ -1,5 +1,6 @@
 # just a brainstorming area where I can play around with different looks for the API
 
+# one example
 results = HTTPMachine.service_access(:web_search => ["paul dix"], :web_search => ["ruby", "nyc"])
 results[:web_search] # => [[return_results, pages], [return_results, pages]]
 
@@ -12,23 +13,34 @@ def self.web_search(*args)
   return [return_results, pages]
 end
 
+# another example
 @results = []
 @pages = []
 HTTPMachine.service_access do
   YahooBOSS.web_search("paul dix") do |results|
-    results.each {|result| @result[result.id]}
-    
+    @results += results
+
     results.each do |result|
-      Page.get(result.url) {|page| @pages << page}
+      Page.get(result.url) {|page| @pages << page }
     end
   end
   
   YahooBOSS.web_search("joe") do |results|
-    @results << results
-    
+    @results += results
+    @
     results.each do |result|
       Page.get(result.url) {|page| @pages << page}
     end
   end
 end
 
+# yet another example
+@results = []
+HTTPMachine.service_access do
+  @results += YahooBOSS.web_search("paul dix").get_pages
+  @results += YahooBOSS.web_search("feedzirra").get_pages
+end
+
+HTTPMachine.service_access do
+  YahooBOSS.remote(:web_search, :params => {:q => "paul dix"})
+end
