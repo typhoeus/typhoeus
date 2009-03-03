128fa127ca74ece257961b40ab320352c730ab60
Paul Dix <paul@pauldix.net>
2009-03-03 10:06:42 -0500

    updated httpmachine with a few methods that will be used across the lib

--- lib/http-machine.rb
+++ lib/http-machine.rb
@@ -2,15 +2,25 @@ $LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirna
 
 require 'curb'
 
+require 'core_ext/proc'
+
 require 'http-machine/remote'
-require 'http-machine/isolated_block'
 
 module HTTPMachine
   VERSION = "0.0.1"
+
+  def self.multi_running?
+    !Thread.current[:curl_multi].nil?
+  end
   
-  def self.service_access
+  def self.add_easy_request(easy_object)
+    Thread.current[:curl_multi].add(easy_object)
+  end
+  
+  def self.service_access(&block)
     Thread.current[:curl_multi] ||= Curl::Multi.new
-    yield
+    block.call
     Thread.current[:curl_multi].perform
+    Thread.current[:curl_multi] = nil
   end
 end
