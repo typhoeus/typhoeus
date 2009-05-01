31d08b26d1b38fa63e5532639bc800069b4244d8
Paul Dix <paul@pauldix.net>
2009-04-30 21:36:05 -0400

    removed all the old outdated example code

--- examples/twitter.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../lib/http-machine.rb'
-
-class Twitter
-  include HTTPMachine
-  remote_method :search, 
-    :server => "http://search.twitter.com/search.json", 
-    :method => :get,
-    :response_type => :json
-end
