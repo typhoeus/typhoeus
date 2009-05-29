7df85b4d3aaf2d4b5f9f1e381feb9f5175cc968b
Paul Dix <paul@pauldix.net>
2009-05-29 17:10:13 -0400

    bumped version for github gem build

--- lib/typhoeus.rb
+++ lib/typhoeus.rb
@@ -12,7 +12,7 @@ require 'typhoeus/remote_proxy_object'
 require 'typhoeus/response'
 
 module Typhoeus
-  VERSION = "0.0.8"
+  VERSION = "0.0.9"
   
   def self.easy_object_pool
     @easy_objects ||= []
