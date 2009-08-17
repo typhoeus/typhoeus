310a86f17481614b2447d1a672dee74aa62b7e04
Paul Dix <paul@pauldix.net>
2009-08-17 12:29:36 -0400

    bumped version for build

--- lib/typhoeus.rb
+++ lib/typhoeus.rb
@@ -12,7 +12,7 @@ require 'typhoeus/remote_proxy_object'
 require 'typhoeus/response'
 
 module Typhoeus
-  VERSION = "0.0.16"
+  VERSION = "0.0.17"
   
   def self.easy_object_pool
     @easy_objects ||= []
