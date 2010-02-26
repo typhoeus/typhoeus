2482b814a4d5f095be10bcbd338c7e98087466ee
Paul Dix <paul@pauldix.net>
2010-02-25 19:12:53 -0500

    added code to parse headers into a hash

--- lib/typhoeus/response.rb
+++ lib/typhoeus/response.rb
@@ -15,5 +15,17 @@ module Typhoeus
       @start_time            = params[:start_time]
       @request               = params[:request]
     end
+    
+    def headers_hash
+      headers.split("\n").map {|o| o.strip}.inject({}) do |hash, o|
+        if o.empty?
+          hash
+        else
+          o = o.split(":")
+          hash[o.first.strip] = o.last ? o.last.strip : nil
+          hash
+        end
+      end
+    end
   end
 end
--- spec/typhoeus/response_spec.rb
+++ spec/typhoeus/response_spec.rb
@@ -33,4 +33,13 @@ describe Typhoeus::Response do
       response.request.should == "whatever"
     end
   end
+  
+  describe "headers" do
+    it "can parse the headers into a hash" do
+      response = Typhoeus::Response.new(:headers => "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nConnection: close\r\nStatus: 200\r\nX-Powered-By: Phusion Passenger (mod_rails/mod_rack) 2.2.9\r\nX-Cache: miss\r\nX-Runtime: 184\r\nETag: e001d08d9354ab7bc7c27a00163a3afa\r\nCache-Control: private, max-age=0, must-revalidate\r\nContent-Length: 4725\r\nSet-Cookie: _some_session=BAh7CDoGciIAOg9zZXNzaW9uX2lkIiU1OTQ2OTcwMjljMWM5ZTQwODU1NjQwYTViMmQxMTkxMjoGcyIKL2NhcnQ%3D--b4c4663932243090c961bb93d4ad5e4327064730; path=/; HttpOnly\r\nServer: nginx/0.6.37 + Phusion Passenger 2.2.4 (mod_rails/mod_rack)\r\nP3P: CP=\"NOI DSP COR NID ADMa OPTa OUR NOR\"\r\n\r\n")
+      response.headers_hash["Status"].should == "200"
+      response.headers_hash["Set-Cookie"].should == "_some_session=BAh7CDoGciIAOg9zZXNzaW9uX2lkIiU1OTQ2OTcwMjljMWM5ZTQwODU1NjQwYTViMmQxMTkxMjoGcyIKL2NhcnQ%3D--b4c4663932243090c961bb93d4ad5e4327064730; path=/; HttpOnly"
+      response.headers_hash["Content-Type"].should == "text/html; charset=utf-8"
+    end
+  end
 end
