5333698fc569fa48fe33603b38dee38989a7fd84
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2010-06-04 00:50:55 +0200

    Add support for SSL options in Easy
    SSL options including :
    
    ssl_cert=
    ssl_key=
    ssl_cacert=
    ssl_capath=
    
    refs to http://github.com/pauldix/typhoeus/issues/#issue/25

--- lib/typhoeus/multi.rb
+++ lib/typhoeus/multi.rb
@@ -9,19 +9,19 @@ module Typhoeus
     def remove(easy)
       multi_remove_handle(easy)
     end
-    
+
     def add(easy)
       @easy_handles << easy
       multi_add_handle(easy)
     end
-    
+
     def perform()
       while active_handle_count > 0 do
         multi_perform
       end
       reset_easy_handles
     end
-    
+
     def cleanup()
       multi_cleanup
     end
