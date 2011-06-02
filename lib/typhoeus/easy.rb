039b13724d7b8a7757992d1e50756c58ec64c687
Yasith Fernando <thekindofme@gmail.com>
2011-06-02 21:33:17 +0800

    Add a new option (disable_ssl_peer_verification) to the request class. Which will set the libcurl option SSL_VERIFYHOST to 0.
    This can help if you trying to access x.y.host.com and the certificate
    is not valid for x but only for y.host.com...etc

--- lib/typhoeus/easy.rb
+++ lib/typhoeus/easy.rb
@@ -33,6 +33,7 @@ module Typhoeus
       :CURLOPT_PROXYTYPE      => 101,
       :CURLOPT_PROXYAUTH      => 111,
       :CURLOPT_VERIFYPEER     => 64,
+      :CURLOPT_VERIFYHOST    => 81,
       :CURLOPT_NOBODY         => 44,
       :CURLOPT_ENCODING       => 10000 + 102,
       :CURLOPT_SSLCERT        => 10025,
@@ -196,6 +197,10 @@ module Typhoeus
 
     def disable_ssl_peer_verification
       set_option(OPTION_VALUES[:CURLOPT_VERIFYPEER], 0)
+    end
+
+    def disable_ssl_host_verification
+      set_option(OPTION_VALUES[:CURLOPT_VERIFYHOST], 0)
     end
 
     def method=(method)
