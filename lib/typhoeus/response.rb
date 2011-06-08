e3499fa9585ae80d261977ce87219975c260856a
Alexis Dorais-Joncas <dorais@eset.sk>
2011-06-08 15:46:40 -0400

    Fix and tests for issue 98

--- lib/typhoeus/response.rb
+++ lib/typhoeus/response.rb
@@ -62,8 +62,20 @@ module Typhoeus
     end
 
     def status_message
-      # http://rubular.com/r/eAr1oVYsVa
-      @status_message ||= first_header_line ? first_header_line[/\d{3} (.*)$/, 1].chomp : nil
+      return @status_message if @status_message 
+
+      # HTTP servers can choose not to include the explanation to HTTP codes. The RFC
+      # states this (http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4):
+      # Except when responding to a HEAD request, the server SHOULD include an entity containing
+      # an explanation of the error situation [...]
+      # This means 'HTTP/1.1 404' is as valid as 'HTTP/1.1 404 Not Found' and we have to handle it.
+
+      # Regexp doc: http://rubular.com/r/eAr1oVYsVa
+      if first_header_line != nil and first_header_line[/\d{3} (.*)$/, 1] != nil
+        @status_message = first_header_line[/\d{3} (.*)$/, 1].chomp
+      else
+        @status_message = nil
+      end
     end
 
     def http_version
