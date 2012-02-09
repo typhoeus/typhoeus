515d20a52d60c0529d2812f905638fd9183fe96b
Daniel Cavanagh <danielcavanagh85@gmail.com>
2012-02-09 17:09:53 +1000

    make the build system use the standard lib and include paths and flags on windows, rather than hardcoding the location of libcurl, and make it link to the dynamic curl libraries as that it what it gets compiled for. this allows typhoeus to be built and run on windows

--- ext/typhoeus/extconf.rb
+++ ext/typhoeus/extconf.rb
@@ -27,39 +27,27 @@ $LIBPATH << "/opt/local/lib" if use_macports
 
 $CFLAGS << " -O3 -Wall -Wcast-qual -Wwrite-strings -Wconversion -Wmissing-noreturn -Winline"
 
-if Config::CONFIG['target_os'] == 'mingw32'
-  header = File.join(ROOT, 'cross', 'curl-7.19.4.win32', 'include')
-  unless find_header('curl/curl.h', header)
-    abort "need libcurl"
-  end
-else
-  HEADER_DIRS = [
-    File.join(INCLUDEDIR, "curl"),
-    INCLUDEDIR,
-    '/usr/include/curl',
-    '/usr/local/include/curl'
-  ]
+HEADER_DIRS = [
+  File.join(INCLUDEDIR, "curl"),
+  INCLUDEDIR,
+  '/usr/include/curl',
+  '/usr/local/include/curl'
+]
 
-  [
-    '/opt/local/include/curl',
-    '/opt/local/include',
-  ].each { |x| HEADER_DIRS.unshift(x) } if use_macports
+[
+  '/opt/local/include/curl',
+  '/opt/local/include',
+].each { |x| HEADER_DIRS.unshift(x) } if use_macports
 
-  unless find_header('curl/curl.h', *HEADER_DIRS)
-    abort "need libcurl"
-  end
+unless find_header('curl/curl.h', *HEADER_DIRS)
+  abort "need libcurl"
 end
 
-if Config::CONFIG['target_os'] == 'mingw32'
-  find_library('curl', 'curl_easy_init',
-               File.join(ROOT, 'cross', 'curl-7.19.4.win32', 'bin'))
-else
-  find_library('curl', 'curl_easy_init',
-               LIBDIR,
-               '/opt/local/lib',
-               '/usr/local/lib',
-               '/usr/lib'
-    )
-end
+find_library(Config::CONFIG['target_os'] == 'mingw32' ? 'curldll' : 'curl', 'curl_easy_init',
+             LIBDIR,
+             '/opt/local/lib',
+             '/usr/local/lib',
+             '/usr/lib'
+  )
 
 create_makefile("typhoeus/native")
