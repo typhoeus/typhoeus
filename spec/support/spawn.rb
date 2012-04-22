e005de279fca9e1c7c69ff042bca96bd890fd98f
Daniel Cavanagh <danielcavanagh85@gmail.com>
2012-04-23 09:03:19 +1000

    make spawn on 1.8 actually return the child pid

--- spec/support/spawn.rb
+++ spec/support/spawn.rb
@@ -8,9 +8,10 @@ if RUBY_PLATFORM == 'java'
 elsif RUBY_VERSION =~ /1.8/
   module Kernel
     def spawn(*args)
-      if fork
+      if pid = fork
       else exec(*args)
       end
+      pid
     end
   end
 end
