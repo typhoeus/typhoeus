2e5f17b46073ca5c2d7a3843fee3aea14737122c
Hans Hasselberg <me@hans.io>
2012-05-14 16:46:35 +0200

    Refactor header spec.

--- spec/typhoeus/header_spec.rb
+++ spec/typhoeus/header_spec.rb
@@ -1,41 +1,42 @@
-require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
+require 'spec_helper'
 
-describe Typhoeus::NormalizedHeaderHash do
-  before(:all) do
-    @klass = Typhoeus::NormalizedHeaderHash
+describe Typhoeus::Header do
+  let(:options) { {} }
+  let(:header) { described_class.new(options) }
+  let(:variations) {
+    [ 'Content-Type', 'content-type', 'cOnTent-TYPE',
+      'Content-Type', 'Content-Type', 'content-type',
+      :content_type ]
+  }
+
+
+  it "responds to has_key?" do
+    header.should respond_to(:has_key?)
   end
 
-  it "should normalize keys on assignment" do
-    hash = @klass.new
-    hash['Content-Type'] = 'text/html'
-    hash['content-type'].should == 'text/html'
-    hash[:content_type].should == 'text/html'
-    hash['Accepts'] = 'text/javascript'
-    hash['accepts'].should == 'text/javascript'
+  describe ".new" do
+    let(:options) { { 'Content-Type' => 'text/html', :x_http_header => 'foo', 'X-HTTP-USER' => 'bar' } }
+
+    it "normalizes keys" do
+      header.keys.should eq(['Content-Type', 'X-Http-Header', 'X-Http-User'])
+    end
   end
 
-  it "should normalize the keys on instantiation" do
-    hash = @klass.new('Content-Type' => 'text/html', :x_http_header => 'foo', 'X-HTTP-USER' => 'bar')
-    hash.keys.should =~ ['Content-Type', 'X-Http-Header', 'X-Http-User']
+  describe "#[]=" do
+    it "normalizes keys" do
+      variations.each do |key|
+        header[key] = 'fsda'
+        header['Content-Type'].should eq('fsda')
+      end
+    end
   end
 
-  it "should merge keys correctly" do
-    hash = @klass.new
-    hash.merge!('Content-Type' => 'fdsa')
-    hash['content-type'].should == 'fdsa'
+  describe "#merge" do
+    before { header.merge!('Content-Type' => 'fdsa') }
+
+    it "normalizes keys" do
+      header['content-type'].should == 'fdsa'
+    end
   end
 
-  it "should allow any casing of keys" do
-    hash = @klass.new
-    hash['Content-Type'] = 'fdsa'
-    hash['content-type'].should == 'fdsa'
-    hash['cOnTent-TYPE'].should == 'fdsa'
-    hash['Content-Type'].should == 'fdsa'
-  end
-
-  it "should support has_key?" do
-    hash = @klass.new
-    hash['Content-Type'] = 'fdsa'
-    hash.has_key?('cOntent-Type').should be_true
-  end
 end
