9592ef6319cab732230733684866e8a933ac5a55
Hans Hasselberg <me@hans.io>
2012-05-14 23:07:24 +0200

    Increase number of requests for benchmarking.

--- benchmarks/vs_nethttp.rb
+++ benchmarks/vs_nethttp.rb
@@ -3,11 +3,11 @@ require File.dirname(__FILE__) + '/../lib/typhoeus.rb'
 require 'open-uri'
 require 'benchmark'
 
-calls = 50
+calls = 5000
 url = "http://127.0.0.1:3000/"
 Typhoeus.init_easy_object_pool
 
-Benchmark.bm do |bm|
+Benchmark.bmbm do |bm|
   bm.report("net::http") do
     calls.times do |i|
       open(url+i.to_s)
