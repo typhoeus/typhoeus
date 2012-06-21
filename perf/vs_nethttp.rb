619ddc84c88d790d9e21b45cc2e1075b45c38a3f
Hans Hasselberg <me@hans.io>
2012-06-21 12:22:18 +0200

    Pimp benchmark.

--- perf/vs_nethttp.rb
+++ perf/vs_nethttp.rb
@@ -3,7 +3,6 @@ require 'net/http'
 require 'open-uri'
 require 'benchmark'
 
-calls = 1000
 URL = "http://localhost:300"
 Typhoeus.init_easy_object_pool
 Typhoeus::Hydra.hydra = Typhoeus::Hydra.new(max_concurrency: 3)
@@ -13,36 +12,45 @@ def url_for(i)
 end
 
 Benchmark.bm do |bm|
-  bm.report("net/http     ") do
-    calls.times do |i|
-      uri = URI.parse(url_for(i))
-      Net::HTTP.get_response(uri)
+
+  [1000].each do |calls|
+    puts "[ #{calls} requests ]"
+
+    bm.report("net/http     ") do
+      calls.times do |i|
+        uri = URI.parse(url_for(i))
+        Net::HTTP.get_response(uri)
+      end
+    end
+
+    bm.report("open         ") do
+      calls.times do |i|
+        open(url_for(i))
+      end
+    end
+
+    bm.report("request      ") do
+      calls.times do |i|
+        Typhoeus::Request.get(url_for(i), {})
+      end
+    end
+
+    bm.report("hydra        ") do
+      calls.times do |i|
+        Typhoeus::Hydra.hydra.queue(Typhoeus::Request.new(url_for(i)))
+      end
+      Typhoeus::Hydra.hydra.run
     end
   end
 
-  bm.report("open         ") do
-    calls.times do |i|
-      open(url_for(i))
-    end
-  end
+  [3].each do |calls|
+    puts "[ #{calls} delayed requests ]"
 
-  bm.report("request      ") do
-    calls.times do |i|
-      Typhoeus::Request.get(url_for(i), {})
+    bm.report("delayed hydra") do
+      calls.times do |i|
+        Typhoeus::Hydra.hydra.queue(Typhoeus::Request.new("localhost:3001/i?delay=1"))
+      end
+      Typhoeus::Hydra.hydra.run
     end
-  end
-
-  bm.report("hydra        ") do
-    calls.times do |i|
-      Typhoeus::Hydra.hydra.queue(Typhoeus::Request.new(url_for(i)))
-    end
-    Typhoeus::Hydra.hydra.run
-  end
-
-  bm.report("delayed hydra") do
-    3.times do |i|
-      Typhoeus::Hydra.hydra.queue(Typhoeus::Request.new("localhost:3001/i?delay=1"))
-    end
-    Typhoeus::Hydra.hydra.run
   end
 end
