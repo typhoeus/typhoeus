2932731023537e88071f08c2dee1f3485640e641
Hans Hasselberg <me@hans.io>
2012-06-22 17:03:51 +0200

    Add specs for Informations.

--- lib/typhoeus/responses/informations.rb
+++ lib/typhoeus/responses/informations.rb
@@ -1,9 +1,10 @@
 module Typhoeus
   module Responses
     module Informations
-      (
-        Ethon::Easies::Informations::AVAILABLE_INFORMATIONS.keys+[:return_code, :response_body, :response_headers]
-      ).each do |name|
+      AVAILABLE_INFORMATIONS = Ethon::Easies::Informations::AVAILABLE_INFORMATIONS.keys+
+        [:return_code, :response_body, :response_headers]
+
+      AVAILABLE_INFORMATIONS.each do |name|
         define_method(name) do
           options[name.to_sym]
         end
--- spec/typhoeus/responses/informations_spec.rb
+++ spec/typhoeus/responses/informations_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Typhoeus::Responses::Informations do
-  it
+  let(:response) { Typhoeus::Response.new }
+  Typhoeus::Responses::Informations::AVAILABLE_INFORMATIONS.each do |name|
+    describe name do
+      it "responds to" do
+        response.should respond_to(name)
+      end
+    end
+  end
 end
