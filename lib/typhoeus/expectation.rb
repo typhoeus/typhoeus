f2ed2e4570b9dd51376afab42e28e089e4d4b272
Hans Hasselberg <me@hans.io>
2012-08-22 04:15:21 +0200

    Fix rdoc param.

--- lib/typhoeus/expectation.rb
+++ lib/typhoeus/expectation.rb
@@ -70,7 +70,7 @@ module Typhoeus
     # @example Check if request matches.
     #   expectation.matches? request
     #
-    # @param [ Request ] The request to check.
+    # @param [ Request ] request The request to check.
     #
     # @return [ Boolean ] True when matches, else false.
     def matches?(request)
