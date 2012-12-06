956aa66b06b8067e17f34e4f382c43f9da165db5
Dwayne Macgowan <dwaynemac@gmail.com>
2012-12-06 01:56:57 -0300

    middleware to decode arrays

---
+++ middleware/typhoeus_array_decoder.rb
@@ -0,0 +1,35 @@
+require '../lib/typhoeus/server_helpers/array_decoder'
+class DecoderMiddleware
+  include Typhoeus::ServerHelpers::ArrayDecoder
+
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    req = Rack::Request.new(env)
+    deep_decode(req.params).each_pair { |k, v| update_params req, k, v }
+    @app.call(env)
+  end
+
+  private
+
+  ##
+  # Persist params change in environment. Extracted from:
+  # https://github.com/rack/rack/blob/master/lib/rack/request.rb#L233
+  #
+  def update_params(req, k, v)
+    found = false
+    if req.GET.has_key?(k)
+      found = true
+      req.GET[k] = v
+    end
+    if req.POST.has_key?(k)
+      found = true
+      req.POST[k] = v
+    end
+    unless found
+      req.GET[k] = v
+    end
+  end
+end
