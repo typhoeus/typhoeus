e1f111dc9f56099f0e7a34b6ea7c3b77c04dbbcc
Hans Hasselberg <me@hans.io>
2013-01-24 13:15:14 +0100

    Update the benchmark.

--- perf/vs_nethttp.rb
+++ perf/vs_nethttp.rb
@@ -4,7 +4,18 @@ require 'open-uri'
 require 'benchmark'
 
 URL = "http://localhost:300"
-Typhoeus::Hydra.hydra = Typhoeus::Hydra.new(max_concurrency: 3)
+hydra = Typhoeus::Hydra.new(max_concurrency: 3)
+
+if defined? require_relative
+  require_relative '../spec/support/localhost_server.rb'
+  require_relative '../spec/support/server.rb'
+else
+  require '../spec/support/localhost_server.rb'
+  require '../spec/support/server.rb'
+end
+LocalhostServer.new(TESTSERVER.new, 3000)
+LocalhostServer.new(TESTSERVER.new, 3001)
+LocalhostServer.new(TESTSERVER.new, 3002)
 
 def url_for(i)
   "#{URL}#{i%3}/"
@@ -15,50 +26,39 @@ Benchmark.bm do |bm|
   [1000].each do |calls|
     puts "[ #{calls} requests ]"
 
-    bm.report("net/http     ") do
+    bm.report("net/http        ") do
       calls.times do |i|
         uri = URI.parse(url_for(i))
         Net::HTTP.get_response(uri)
       end
     end
 
-    bm.report("open         ") do
+    bm.report("open            ") do
       calls.times do |i|
         open(url_for(i))
       end
     end
 
-    bm.report("request      ") do
+    bm.report("request         ") do
       calls.times do |i|
-        Typhoeus::Request.get(url_for(i), {})
+        Typhoeus::Request.get(url_for(i))
       end
     end
 
-    bm.report("hydra        ") do
+    bm.report("hydra           ") do
       calls.times do |i|
-        Typhoeus::Hydra.hydra.queue(Typhoeus::Request.new(url_for(i)))
+        hydra.queue(Typhoeus::Request.new(url_for(i)))
       end
-      Typhoeus::Hydra.hydra.run
+      hydra.run
     end
 
-    bm.report("hydra memoize") do
+    bm.report("hydra memoize   ") do
       Typhoeus::Config.memoize = true
       calls.times do |i|
-        Typhoeus::Hydra.hydra.queue(Typhoeus::Request.new(url_for(i)))
+        hydra.queue(Typhoeus::Request.new(url_for(i)))
       end
-      Typhoeus::Hydra.hydra.run
+      hydra.run
       Typhoeus::Config.memoize = false
-    end
-  end
-
-  [3].each do |calls|
-    puts "[ #{calls} delayed requests ]"
-
-    bm.report("delayed hydra") do
-      calls.times do |i|
-        Typhoeus::Hydra.hydra.queue(Typhoeus::Request.new("localhost:3001/i?delay=1"))
-      end
-      Typhoeus::Hydra.hydra.run
     end
   end
 end
