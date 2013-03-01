92d4e06a4fe52cf5d7eec62a8cd6ab4085104095
Hans Hasselberg <me@hans.io>
2013-03-01 21:38:28 +0100

    Remove unused code.

--- spec/typhoeus/errors/no_stub_spec.rb
+++ spec/typhoeus/errors/no_stub_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe Typhoeus::Errors::NoStub do
   let(:base_url) { "localhost:3001" }
-  let(:options) { {:verbose => true, :headers => { 'User-Agent' => "Fubar" }} }
-  let(:request) { Typhoeus::Request.new(base_url, options) }
+  let(:request) { Typhoeus::Request.new(base_url) }
   let(:message) { "The connection is blocked and no stub defined: " }
 
   subject { Typhoeus::Errors::NoStub }
