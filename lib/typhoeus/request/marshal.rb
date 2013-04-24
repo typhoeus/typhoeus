e221524c49af499bf389de9b7229f8af184a895b
Jan Rubio <jan.rubio@opower.com>
2013-04-23 22:02:29 -0400

    Updating marshal to exclude hydra

--- lib/typhoeus/request/marshal.rb
+++ lib/typhoeus/request/marshal.rb
@@ -5,10 +5,10 @@ module Typhoeus
     module Marshal
 
       # Return the important data needed to serialize this Request, except the
-      # `on_complete`, `on_success`, and `on_failure` handlers, since they cannot be marshalled.
+      # `on_complete`, `on_success`, `on_failure`, and `hydra`, since they cannot be marshalled.
       def marshal_dump
-        callbacks = %w(@on_complete @on_success @on_failure)
-        (instance_variables - callbacks - callbacks.map(&:to_sym)).map do |name|
+        unmarshallable = %w(@on_complete @on_success @on_failure @hydra)
+        (instance_variables - unmarshallable - unmarshallable.map(&:to_sym)).map do |name|
           [name, instance_variable_get(name)]
         end
       end
