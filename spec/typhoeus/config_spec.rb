72d1c745cc5ded46a31cdff0bceac4c483bbb9a0
Lukas Rieder <l.rieder@gmail.com>
2016-07-04 12:31:05 +0200

    allow global proxy setting using Typhoeus::Config.proxy = "http://some-proxy-server.net"

--- spec/typhoeus/config_spec.rb
+++ spec/typhoeus/config_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Typhoeus::Config do
   let(:config) { Typhoeus::Config }
 
-  [:block_connection, :memoize, :verbose, :cache, :user_agent].each do |name|
+  [:block_connection, :memoize, :verbose, :cache, :user_agent, :proxy].each do |name|
     it "responds to #{name}" do
       expect(config).to respond_to(name)
     end
