23e676c37c96fb4120bf33fe61dd7c19c5450ce1
Tors Dalid <torsdalid@gmail.com>
2019-07-02 17:03:45 +0200

    Use Request caching mechanism in Hydra
    This is related to https://github.com/typhoeus/typhoeus/pull/601 but applied in Hydra queued requests.

--- spec/typhoeus/hydra/cacheable_spec.rb
+++ spec/typhoeus/hydra/cacheable_spec.rb
@@ -4,6 +4,7 @@ describe Typhoeus::Hydra::Cacheable do
   let(:base_url) { "localhost:3001" }
   let(:hydra) { Typhoeus::Hydra.new() }
   let(:request) { Typhoeus::Request.new(base_url, {:method => :get}) }
+  let(:response) { Typhoeus::Response.new }
   let(:cache) { MemoryCache.new }
 
   describe "add" do
@@ -24,7 +25,6 @@ describe Typhoeus::Hydra::Cacheable do
       end
 
       context "when request in memory" do
-        let(:response) { Typhoeus::Response.new }
         before { cache.memory[request] = response }
 
         it "returns response with cached status" do
@@ -53,6 +53,31 @@ describe Typhoeus::Hydra::Cacheable do
           end
         end
       end
+
+      context "when cache is specified on a request" do
+        before { Typhoeus::Config.cache = false }
+
+        context "when cache is false" do
+          let(:non_cached_request) { Typhoeus::Request.new(base_url, {:method => :get, :cache => false}) }
+
+          it "finishes request" do
+            expect(non_cached_request.response).to_not be_truthy
+            hydra.add(non_cached_request)
+          end
+        end
+
+        context "when cache is defined" do
+          let(:cached_request) { Typhoeus::Request.new(base_url, {:method => :get, :cache => cache}) }
+
+          before { cache.memory[cached_request] = response }
+
+          it "finishes request" do
+            expect(cached_request).to receive(:finish).with(response)
+            hydra.add(cached_request)
+          end
+        end
+      end
+
     end
   end
 end
