a0dc59f94166b20c1735ad813be12d0ccb0437e6
Katelyn Schiesser <kschiesser@laika.com>
2025-08-18 21:12:51 +0000

    add spec tests for URI base_url

--- spec/typhoeus/expectation_spec.rb
+++ spec/typhoeus/expectation_spec.rb
@@ -211,6 +211,35 @@ describe Typhoeus::Expectation do
       end
     end
 
+    context "when URI" do
+      let(:request_url) { URI("http://www.example.com") }
+      let(:request) { Typhoeus::Request.new(request_url) }
+
+      context "when match" do
+        let(:base_url) { URI("http://www.example.com") }
+
+        it "returns true" do
+          expect(url_match).to be_truthy
+        end
+      end
+
+      context "when no match" do
+        let(:base_url) { URI("http://www.notexample.com") }
+
+        it "returns false" do
+          expect(url_match).to be_falsey
+        end
+
+        context "with nil request_url" do
+          let(:request_url) { nil }
+
+          it "returns false" do
+            expect(url_match).to be_falsey
+          end
+        end
+      end
+    end
+
     context "when nil" do
       let(:base_url) { nil }
 
